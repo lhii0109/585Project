0f0702b5abbce3a47209ad4c1c07f0449fd0a8b3
nbakeraaron <nbakeraaron@gmail.com>
2017-05-04 13:00:59 -0500

    Update 585-Run.R

--- 585-Run.R
+++ 585-Run.R
@@ -1,16 +1,21 @@
-source("background-functions.R")
 
-source("585-Diagnostic.R")
 
 Clusters <- detectCores()-1
 cl <- makePSOCKcluster(Clusters)
 registerDoParallel(cl)
 
-Diagnostic()
+source("backgroundfunctions.R")
+  
+source("585Diagnostic.R")
+  
+Result1 <- Diagnostic(M = 3, K = 80, x = X, y = y, Initialize = 1, Model = "SandProbit", Methodname = "SandwichProbit")
+Result2 <- Diagnostic(M = 3, K = 80, x = X, y = y, Initialize = 1, Model = "ASISProbit", Methodname = "ASISProbit")
 
-ACFPlot()
+Result1
+Result2
 
-GelmanPlot()
+ACFPlot(Result1, Result2)
+GelmanPlot(Result1, Result2, parnames = C("Par[0]", "Par[1]", "Par[2]"), name1 = "SandwichProbit", name2 = "ASISProbit", p = 3)
 
 stopCluster(cl)
 
